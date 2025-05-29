6725e252799e4c519510e4abfa866c2cf1873230
Yahya Ghanem <48291825+JonGhanem@users.noreply.github.com>
2025-05-29 19:18:06 +0300

    Internationalization the request

--- src/main/java/com/ghanem/RestfulWebServicesApplication/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/ghanem/RestfulWebServicesApplication/helloworld/controller/HelloWorldController.java
@@ -1,17 +1,33 @@
 package com.ghanem.RestfulWebServicesApplication.helloworld.controller;
 
 import com.ghanem.RestfulWebServicesApplication.helloworld.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/v1/hello-word")
     public String helloWorld(){
          return "Hello World!";
      }
+
+    @GetMapping(path = "/v1/hello-word-inter")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
+    }
+
 
     @GetMapping(path = "/v1/hello-word-bean")
     public HelloWorldBean helloWorldBean(){
