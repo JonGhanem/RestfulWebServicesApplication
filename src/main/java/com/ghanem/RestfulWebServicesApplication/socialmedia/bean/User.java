8bdcff44a2e003c1b37eed239cb111d9a61c790b
Yahya Ghanem <48291825+JonGhanem@users.noreply.github.com>
2025-05-29 18:12:48 +0300

    commit user
    added validation
    added Swagger UI
    added Exception Handler

---
+++ src/main/java/com/ghanem/RestfulWebServicesApplication/socialmedia/bean/User.java
@@ -0,0 +1,59 @@
+package com.ghanem.RestfulWebServicesApplication.socialmedia.bean;
+
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class User {
+    private Integer id;
+
+    @Size(min = 3)
+    private String name;
+    @Past
+    private LocalDate birthDate;
+
+    public User(Integer id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(birthDate, user.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, birthDate);
+    }
+}
---
+++ src/main/java/com/ghanem/RestfulWebServicesApplication/socialmedia/controller/UserResource.java
@@ -0,0 +1,58 @@
+package com.ghanem.RestfulWebServicesApplication.socialmedia.controller;
+
+import com.ghanem.RestfulWebServicesApplication.socialmedia.bean.User;
+import com.ghanem.RestfulWebServicesApplication.socialmedia.dao.UserDaoService;
+import com.ghanem.RestfulWebServicesApplication.socialmedia.exception.UserNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveOneUser(){
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/user/{id}")
+    public User retrieveOneUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user==null)
+            throw new UserNotFoundException("id:"+id);
+        return user;
+    }
+
+
+    @PostMapping("/create-user")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+
+        User savedUser = service.save(user);
+        // return in the header
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(path = "/user/{id}")
+    public void DeleteOneUser(@PathVariable int id){
+        service.deleteById(id);
+    }
+
+}
